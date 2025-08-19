aa2416656fcb69f6a8d466aad05de1ecf7eb971e
FrancoDev7 <francisco.pereira16@inacapmail.cl>
2025-08-19 15:39:54 -0400

    Hook useRef

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 import type { Gif } from "../interfaces/gif.interface";
+
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
+  const gifsCache = useRef<Record<string, Gif[]>>({});
+
   const handleTermClicked = async (term: string) => {
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
+      return;
+    }
+
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
   };
@@ -20,6 +29,8 @@ export const useGifs = () => {
     setPreviousTerms([query, ...previousTerms].splice(0, 8));
     const gifs = await getGifsByQuery(query);
     setGifs(gifs);
+
+    gifsCache.current[query] = gifs;
   };
   return {
     gifs,
